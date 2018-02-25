2054b17f4edd14be614e7e5b2d429a9154c9dbac
Johnathan Choi <john.choi.447@gmail.com>
2018-02-25 01:19:10 -0800

    fully resolves CAD trade value

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // server.js
 
 var express  = require('express');
-var app      = express();                               // create our app w/ express
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var app      = express();                   
+var morgan = require('morgan');             
+var bodyParser = require('body-parser');    
+var methodOverride = require('method-override'); 
 var xlsx = require('xlsx');
 var fileUpload = require('express-fileupload');
 var fs = require('fs-extra');
@@ -14,11 +14,11 @@ var usdcad = require('./public/lib/usdcad.json');
 var async = require('async');
 
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(express.static(__dirname + '/public'));                 
+app.use(morgan('dev'));                                         
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                     
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 app.use(methodOverride());
 app.use(fileUpload());
 
@@ -58,70 +58,109 @@ app.post('/api/doc', function(req, res) {
     var workbook = xlsx.read(req.files.file.data); //input file to workbook
     var worksheet = workbook.Sheets[workbook.SheetNames[0]]; //workbook sheet1
     var jsonSheet = xlsx.utils.sheet_to_json(worksheet).reverse(); //sheet1 to json. binance sheet is ordered newest -> oldest, so I'm using reverse function
+    
+    var promiseSheet = new Promise(function(resolve, reject) {
 
-    async.forEachSeries(jsonSheet, function(value, callback) {
-        var tradeDate, tradeDate2, tradeMarket, tradeType, tradeTotal, tradeQuery, tradeDateFixer; //declare variables
+    async.forEachOfSeries(jsonSheet, function(value, key, callback) {
+        var tradeDate, tradeDate2, tradeMarket, tradeAlt, tradeType, 
+            tradePrice, tradeAmount, tradeTotal, tradeDateFixer, usdRate, cadRate, 
+            tradeFee, tradeFeeCoin, cadFinal, cadTotal; //declare variables
+
         tradeDate = new Date(value.Date); //start date
         tradeDate2 = new Date(tradeDate.getTime()+(60000)); //end date @ start+1 min
-        tradeDateFixer = getDateTime(new Date(tradeDate), 'date-');
+        tradeDateFixer = getDateTime(new Date(tradeDate), 'date-'); //start date in "-"" notation, used for fixer API
         tradeMarket = value.Market.slice(-3); //trading pair; slice to determine btc/eth
+        tradeAlt = value.Market.substring(0, (value.Market.length)-3); //trading pair; the alt or the initial
         tradeType = value.Type; //buy or sell
+        tradePrice = value.Price;
+        tradeAmount = value.Amount;
+        tradeTotal = value.Total;
+        tradeFee = value.Fee;
+        tradeFeeCoin = value['Fee Coin'];
 
-        var promise = new Promise(function(resolve, reject) {
+        var promiseRate;
+        var promiseRow = new Promise(function(resolve, reject) {
 
-            function getGDAXrate(){
-                //make user-agent header for gdax because they need one
-                var gdaxHeaders = {headers:{'User-Agent':'cgcalc'}};
+            //make user-agent header for gdax because they need one
+            var gdaxHeaders = {headers:{'User-Agent':'cgcalc'}};
 
-                //get ETH or BTC trading price from gdax at time of trade on binance
-                rp.get('https://api.gdax.com/products/'+tradeMarket+'-USD/candles?granularity=60&start='+tradeDate+'&end='+tradeDate2, gdaxHeaders).then(async function(body) {
-                    console.log("---");
-                    console.log(tradeDate);
-                    console.log(tradeMarket +" closing price: "+JSON.parse(body)[0][4]);
+            //get ETH or BTC trading price from gdax at time of trade on binance
+            rp.get('https://api.gdax.com/products/'+tradeMarket+'-USD/candles?granularity=60&start='+tradeDate+'&end='+tradeDate2, gdaxHeaders).then(function(body) {
+                usdRate = JSON.parse(body)[0][4];
 
+                promiseRate = new Promise(function(resolve, reject) {
                     var usdcadSearch = usdcad.find(function(array){
                         return array.date == tradeDateFixer;
                     });
-    
+
                     if (usdcadSearch) {
-                        console.log("CAD: "+usdcadSearch.rate);
+                        cadRate = usdcadSearch.rate;
+                        resolve();
                     } else {
                         rp.get('https://api.fixer.io/'+tradeDateFixer+'?base=USD&symbols=CAD').then(function(body) {
-                            var fixerRate = JSON.parse(body).rates.CAD;
-    
-                            usdcad.push({"date": tradeDateFixer,"rate":fixerRate});
-    
-                            //update the usdcad.json file
-                            fs.writeJson("./public/lib/usdcad.json", usdcad).then(function(body){
-                                console.log("usdcad.json updated");
+                            cadRate = JSON.parse(body).rates.CAD;
+                            usdcad.push({"date": tradeDateFixer,"rate":cadRate});
+
+                            fs.writeJson("./public/lib/usdcad.json", usdcad).then(function(body){ //update usdcad.json
                             }).catch(function(err) {
                                 console.log(err+"fs writefile")
                             });
-    
-                            console.log("FixerCAD: "+fixerRate);
+
+                            resolve();
                         }).catch(function(err) {
                             console.log(err+"fixer request");
+                            reject();
                         });
                     }
-
+                }).then(function() {
                     setTimeout(resolve, 500);
                 }).catch(function(err) {
-                    console.log(err+"gdax request");
+                    console.log(err);
                     reject();
                 });
-            };
 
-            getGDAXrate();
+            }).catch(function(err) {
+                console.log(err+"gdax request");
+                reject();
+            });
         });
 
-        promise.then(function() {
+        Promise.all([promiseRow, promiseRate]).then(function() {
+
+            if(tradeType=="BUY") {
+                if(tradeFeeCoin == tradeAlt) {
+                    tradeTotal = tradePrice * (tradeAmount-tradeFee);
+                }
+                else if (tradeFeeCoin == "BNB") {
+                    tradeTotal = tradeTotal *0.995; //or something
+                }
+                cadFinal = -1*tradeTotal*usdRate*cadRate;
+                console.log("BUY: "+ cadFinal);
+                jsonSheet[key].CADValue = cadFinal;
+
+            } else if(tradeType=="SELL") {
+                if(tradeFeeCoin == tradeMarket){
+                    tradeTotal = tradeTotal - tradeFee;
+                }
+                else if (tradeFeeCoin == "BNB") {
+                    tradeTotal = tradeTotal *0.995; //or something
+                }
+                cadFinal = tradeTotal*usdRate*cadRate;
+                console.log("SELL: " + cadFinal);
+                jsonSheet[key].CADValue = cadFinal;
+            }
+
+            if (key == jsonSheet.length-1) { //when all rows are done
+                res.json(jsonSheet);
+            }
+
             callback();
         }).catch(function() {
             console.log("Promise rejected. Should restart.");
-        });
-    });
+        }); //promiseRow
+    }); //async forEachOfSeries
 
-    res.json(jsonSheet);
+    });
 });
 
 // listen (start app with node server.js) ======================================
